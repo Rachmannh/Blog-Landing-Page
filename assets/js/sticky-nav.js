172da668c9ec8b52643d5dcbc29d3308713e07dd
Rachmannh <rachmannul1999@gmail.com>
2022-08-24 23:12:31 +0700

    commit

--- assets/js/sticky-nav.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector(".navbar-expand-lg");
-window.onscroll = () => {
-  if (window.scrollY > 100) {
-    navbar.classList.add("change");
-  } else {
-    navbar.classList.remove("change");
-  }
-};
